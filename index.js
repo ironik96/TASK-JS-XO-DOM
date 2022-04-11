bb5efe84efbfb6a34720d5a9d91028c5c53f6a65
Ahmed AlAmiri <a.alamiri.96@gmail.com>
2022-04-11 07:46:50 +0300

    Solve task

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
+const X_ELEMENT = "X";
+const O_ELEMENT = "O";
+const NO_ELEMENT = "";
+const WIN_PATTERNS = new Set([
+  ["0", "1", "2"],
+  ["3", "4", "5"],
+  ["6", "7", "8"],
+  ["0", "3", "6"],
+  ["1", "4", "7"],
+  ["2", "5", "8"],
+  ["0", "4", "8"],
+  ["2", "4", "6"],
+]);
+
 // ❗️ DON'T TOUCH THESE 2 FUNCTIONs
 // Pre-made function that will fill the button with its number.
 // First button top left is called 1, last button bottom right is 9
 function fillButton(index, text) {
   // This function fills the button of the send index
-  document.getElementById(index).innerHTML = text;
+  let docElement = document.getElementById(index);
+  docElement.style.color = element === X_ELEMENT ? "green" : "red";
+  docElement.innerHTML = text;
 }
 // pre-made a function. You can use this function to present an alert to say someone wins
 function winningAlert(winner) {
-  if(confirm(`Horraaay, ${winner} wins!`)){
-     // The code here will be exectued if you press on OK button that will pop on the window 
+  if (confirm(`Horraaay, ${winner} wins!`)) {
+    // The code here will be exectued if you press on OK button that will pop on the window
   }
 }
-
-// SAMPLE CODE: This code fills the 1st and 9th button with X and O initially
-// ❗️ Delete this code once you are done testing
-fillButton(1, "X");
-fillButton(9, "O");
 
 /**
  *
@@ -25,12 +36,52 @@ fillButton(9, "O");
  * That interacts with the UI
  */
 function clickButton(index) {
-  console.log(`Button number ${index} is clicked`);
-  // Your main code here.
+  if (board[index - 1] === NO_ELEMENT) {
+    board[index - 1] = element;
+    fillButton(index, element);
+  }
+  element = toggleElement(element);
+  checkWinner(X_ELEMENT);
+  checkWinner(O_ELEMENT);
+  if (!board.includes(NO_ELEMENT)) draw();
 }
 
 /**
  * (Optional) It's always a good idea to make a function for every single purpose.
  */
-// function checkWinner
-// function restartGame
+
+const restartGame = () => {
+  board = [...new Array(9)].map((e) => NO_ELEMENT);
+  element = Math.floor(Math.random() * 2) === 0 ? X_ELEMENT : O_ELEMENT;
+  board.forEach((e, i) => fillButton(i + 1, e));
+};
+
+const toggleElement = (element) =>
+  element === X_ELEMENT ? O_ELEMENT : X_ELEMENT;
+
+const getElementIndices = (element) =>
+  board.map((e, i) => (e === element ? i : "")).join("");
+
+const winner = (element) => {
+  winningAlert(element);
+  restartGame();
+};
+
+const draw = () => {
+  alert("Draw");
+  restartGame();
+};
+
+function checkWinner(element) {
+  const indices = getElementIndices(element);
+  WIN_PATTERNS.forEach((pattern) => {
+    if (pattern.every((pos) => indices.includes(pos))) {
+      winner(element);
+      return;
+    }
+  });
+}
+
+let board;
+let element;
+restartGame();
